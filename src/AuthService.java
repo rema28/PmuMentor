3c37d05d0bd412a9afeb7c5a160daccbc0741c29
Azzah AlOtaishan <alotaishanazzah@gmail.com>
2025-12-14 12:12:52 +0300

    Implement AuthService for user registration and login

--- src/AuthService.java
+++ src/AuthService.java
@@ -1,22 +1,20 @@
 import java.util.HashMap;
-
 public class AuthService {
-  private HashMap<String, User> users = new HashMap<>();
-
-  public boolean register(String username, String password, String email) {
-    if (user.containsKey(username)) {
-      return false;
+    private HashMap<String, User> users = new HashMap<>();
+    public boolean register(String username, String password, String email) {
+        if (users.containsKey(username)) {
+            return false;
+        }
+        User user = new User(username, password, email);
+        users.put(username, user);
+        return true;
     }
-    User user = new User(username, password, email);
-    users.put(username, user);
-    return true;
-  }
 
-public boolean login(String username, String password) {
-  if (!users.containsKey(username)) {
-    return false;
-  }
-  User user = users.get(username);
-  return user.getPassword().equals(password);
-}
+    public boolean login(String username, String password) {
+        if (!users.containsKey(username)) {
+            return false;
+        }
+        User user = users.get(username);
+        return user.getPassword().equals(password);
+    }
 }
